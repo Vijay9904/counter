e74045bcddc24a97c2b8f55fd727ff9c630320d6
vijay <vijaykhetre36@gmail.com>
2024-02-18 22:45:26 +0530

    added your files

--- counter.js
+++ counter.js
@@ -1,13 +1,28 @@
 let count=0;
 const value=document.querySelector("#value");
-const buttons=document.querySelector(".btn");
+const buttons=document.querySelectorAll(".btn");
 buttons.forEach(function(btn) {
-    btn.addEventlistener("click",function(e){
+    btn.addEventListener("click",function(e){
         const st=e.currentTarget.classList;
         if(st.contains('dec')){
             count --;
         }
+       else if(st.contains('inc')){
+            count ++
+        }
+        else{
+            count=0;
+        }
+        if(count>0){
+            value.style.color="green";
+        }
+        if(count<0)
+    {
+        value.style.color="orange";
+    }
+    if(count==0)
+    {
+        value.style.color="black";
+    }
         value.textContent=count;
-    });
-    
-});
+    });});
